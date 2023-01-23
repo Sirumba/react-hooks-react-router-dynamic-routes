5a79f54c7a48ca717418f0e864a9bfac59791e75
Sirumba <simonsmaina45@gmail.com>
2023-01-23 19:56:45 +0300

    changes in moviespage

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
+import MoviesList from "./MoviesList";
+import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
-  const renderMovies = Object.keys(movies).map((movieID) => (
-    <li key={movieID}>
-      <Link to={`/movies/${movieID}`}>{movies[movieID].title}</Link>
-    </li>
-  ));
+  const match = useRouteMatch();
+  console.log(match)
 
-    return <ul>{renderMovies}</ul>;
+  return (
+    <div>
+      <MoviesList movies={movies} />
 
+      <Route exact path={match.url}>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+      
+      <Route path={`${match.url}/:movieId`}>
+        <MovieShow movies={movies} />
+      </Route>
+    </div>
+  );
 }
 
 export default MoviesPage;
